f018cdcc684ce08dd06a8a57b741e3534964a0a0
Andreas Frederiksen <Andreas020299@gmail.com>
2019-11-26 11:36:27 +0100

    NewInfo

--- ProSoLoPortal/Controllers/CaseController.cs
+++ ProSoLoPortal/Controllers/CaseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +13,14 @@ namespace ProSoLoPortal.Controllers
 {
     public class CaseController : Controller
     {
+        private readonly UserManager<ApplicationUser> UserManager;
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> SignInManager;
 
-        public CaseController(ApplicationDbContext context)
+        public CaseController(ApplicationDbContext context, UserManager<ApplicationUser> UserManager, SignInManager<ApplicationUser> SignInManager)
         {
+            this.UserManager = UserManager;
+            this.SignInManager = SignInManager;
             _context = context;
         }
 
@@ -54,12 +59,63 @@ namespace ProSoLoPortal.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CaseId,Name,TimeFrame,NumberOfProducts,Seller,ProposedPrice,UserRefId,IsLocked,IsFinished,TimeFrameFexible")] Case @case)
+        public async Task<IActionResult> Create([Bind("CaseId,Name,TimeFrame,NumberOfProducts,Seller,ProposedPrice,IsLocked,IsFinished,TimeFrameFexible")] Case @case)
         {
             if (ModelState.IsValid)
             {
+                var CurrentUser = await UserManager.GetUserAsync(HttpContext.User);
+                @case.UserRefId = CurrentUser.Id;
                 _context.Add(@case);
                 await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(@case);
+        }
+        public async Task<IActionResult> Bid(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @case = await _context.Case.FindAsync(id);
+            if (@case == null)
+            {
+                return NotFound();
+            }
+            return View(@case);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CaseId")] Case @case, )
+        {
+            if (id != @case.CaseId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Bids bid = new Bids
+                    {
+                        BidPrice =
+                    };
+                    _context.Update(@case);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CaseExists(@case.CaseId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(@case);
