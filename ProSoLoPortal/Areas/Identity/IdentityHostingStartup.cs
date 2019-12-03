048c8866154d6952bcd263d862e246823b5c06ab
Mikkel Høyberg <43138794+mikk3l1@users.noreply.github.com>
2019-12-03 13:48:35 +0100

    Added WebLogo and some identity classes
    Looking into how to restrict identity access to different pages
    And added WebLogo for the website

---
+++ ProSoLoPortal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProSoLoPortal.Data;
+using ProSoLoPortal.Models;
+
+[assembly: HostingStartup(typeof(ProSoLoPortal.Areas.Identity.IdentityHostingStartup))]
+namespace ProSoLoPortal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
